83438b5f5b5afe157c08b2e8f61052c3acc983c1
Tumeyy <stefan.tumey@gmail.com>
2022-09-23 23:44:53 -0400

    Set up db connection string in config, registered sql connection to service builder

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region database
+var sqlConnection = new SqlConnectionStringBuilder();
+sqlConnection.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConnection.UserID = builder.Configuration["UserId"];
+sqlConnection.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnection.ConnectionString));
+
+#endregion
 
 var app = builder.Build();
 
